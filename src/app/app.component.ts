353729aac014f6f9104eb5b30b5ed8cb9cdc5a4c
marascampini <mara.scampini@gmail.com>
2023-09-15 17:10:26 +0200

    Generate password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { parse } from 'path';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'udemy-test';
+  title = 'Password Generator';
+  password: string = '';
+  useLetters: boolean = false;
+  useNumbers: boolean = false;
+  useSymbols: boolean = false;
+  length: number = 0;
+
+  onChangeLetters() {
+    this.useLetters = !this.useLetters;
+  }
+
+  onChangeNumbers() {
+    this.useNumbers = !this.useNumbers;
+  }
+
+  onChangeSymbols() {
+    this.useSymbols = !this.useSymbols;
+  }
+
+  onChangeLength(event: Event) {
+    const parsedValue = parseInt((event.target as HTMLInputElement).value);
+
+    if(!isNaN(parsedValue)){
+      this.length = parsedValue;
+    }
+  }
+
+  onButtonClick() {
+    this.password = 'My Password';
+  }
 }
